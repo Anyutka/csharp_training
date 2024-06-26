a0974a0592bddee12d42ab3318aa54a3e827c982
Anya <anyuta.kirienko@gmail.com>
2024-06-26 21:23:50 +0300

    Шаблон проектирования Singleton и его многопоточная вариация

--- addressbook-web-tests/addressbook-web-tests/appmanager/ApplicationManager.cs
+++ addressbook-web-tests/addressbook-web-tests/appmanager/ApplicationManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
@@ -15,15 +16,14 @@ namespace WebAddressbookTests
     {
         protected IWebDriver driver;
         protected string baseURL;
-        
-
+   
         protected LoginHelper loginHelper;
         protected NavigationHelper navigator;
         protected GroupHelper groupHelper;
         protected ContactHelper contactHelper;
+        private static ThreadLocal<ApplicationManager> app = new ThreadLocal<ApplicationManager>();
 
-
-        public ApplicationManager()
+        private ApplicationManager()
         {
             driver = new FirefoxDriver();
             baseURL = "http://localhost/addressbook";
@@ -32,11 +32,7 @@ namespace WebAddressbookTests
             groupHelper = new GroupHelper(this);
             contactHelper = new ContactHelper(this);
         }
-        public IWebDriver Driver 
-        {
-            get {  return driver; }
-        }
-        public void Stop()
+        ~ApplicationManager()
         {
             try
             {
@@ -46,8 +42,21 @@ namespace WebAddressbookTests
             {
                 // Ignore errors if unable to close the browser
             }
-            
+
         }
+        public static ApplicationManager GetInstance()
+        {
+            if (! app.IsValueCreated)
+            {
+                app.Value = new ApplicationManager();
+            }
+            return app.Value;
+        }
+        public IWebDriver Driver 
+        {
+            get {  return driver; }
+        }
+        
         public LoginHelper Auth
         {
             get
