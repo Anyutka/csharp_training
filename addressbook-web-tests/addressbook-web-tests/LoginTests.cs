e332b15950381245a4cc272ac83bed65a6534db2
Anya <anyuta.kirienko@gmail.com>
2024-06-28 14:05:52 +0300

    Добавляем тесты для login/logout

---
+++ addressbook-web-tests/addressbook-web-tests/LoginTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class LoginTests : TestBase
+    {
+        [Test]
+        public void LoginWithValidCredentials()
+        {
+            //prepare test situation
+            app.Auth.Logout();
+            //action
+            AccountData account = new AccountData("admin", "secret");
+            app.Auth.Login(account);
+            //verification
+            Assert.IsTrue(app.Auth.IsLoggedIn(account));
+        }        
+        [Test]
+        public void LoginWithInvalidCredentials()
+        {
+            //prepare test situation
+            app.Auth.Logout();
+            //action
+            AccountData account = new AccountData("admin", "1234");
+            app.Auth.Login(account);
+            //verification
+            Assert.IsFalse(app.Auth.IsLoggedIn(account));
+        }
+    }
+}
--- addressbook-web-tests/addressbook-web-tests/appmanager/LoginHelper.cs
+++ addressbook-web-tests/addressbook-web-tests/appmanager/LoginHelper.cs
@@ -12,16 +12,43 @@ namespace WebAddressbookTests
 {
     public class LoginHelper : HelperBase
     {
-        
+
         public LoginHelper(ApplicationManager manager) : base(manager)
-    {
-    }
-    
-    public void Login(AccountData account)
         {
+        }
+
+        public void Login(AccountData account)
+        {
+            if (IsLoggedIn())
+            {
+                if (IsLoggedIn(account))
+                {
+                    return;
+                }
+                Logout();                
+            }
             Type(By.Name("user"), account.Username);
-            Type(By.Name("pass"), account.Password);                   
+            Type(By.Name("pass"), account.Password);
             driver.FindElement(By.XPath("//input[@value='Login']")).Click();
+        }
+
+        public void Logout()
+        {
+            if (IsLoggedIn())
+            {
+                driver.FindElement(By.LinkText("Logout")).Click();
+            }
+            
+        }
+        public bool IsLoggedIn()
+        {
+            return IsElementPresent(By.Name("logout"));
+        }
+        public bool IsLoggedIn(AccountData account)
+        {
+            return IsLoggedIn()
+            && driver.FindElement(By.Name("logout")).FindElement(By.TagName("b")).
+            Text ==("(" + account.Username + ")");
         }
     }
 }
