1754e57170e6613e57fe3bde37f82044312c1624
Anya <anyuta.kirienko@gmail.com>
2024-10-11 16:42:36 +0300

    Задание 18 with white only creation tests

---
+++ addressbook_tests_white/addressbook_tests_white/tests/TestBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestStack.White;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.WindowItems;
+
+namespace addressbook_tests_white
+{
+    public class TestBase
+
+    {
+        public ApplicationManager app;
+
+        [TestFixtureSetUp]
+        public void initApplication()
+        {
+            app = new ApplicationManager();
+        }
+        
+        [TestFixtureTearDown]
+
+        public void stopApplication()
+        {
+            app.Stop();
+        }
+
+    }   
+    
+}
